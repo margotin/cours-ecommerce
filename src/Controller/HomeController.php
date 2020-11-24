fcb1761102f73d472310ef5c78fa8dc9f1817897
Mathieu Margotin <mathieu.margotin@gmail.com>
2020-11-24 15:09:52 +0100

    creation entites Product et Category

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")"
      */
-    public function homepage(){
+    public function homepage()
+    {
         return $this->render('home.html.twig');
     }
 }
